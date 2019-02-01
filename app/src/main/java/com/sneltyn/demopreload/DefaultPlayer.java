f71ab0135587bcb0f7aca8cf00318cdbf882ea21
sneltyn <sneltyn@gmail.com>
2019-02-01 20:07:54 +0200

    Need release player after error

--- app/src/main/java/com/sneltyn/demopreload/DefaultPlayer.java
+++ app/src/main/java/com/sneltyn/demopreload/DefaultPlayer.java
@@ -1,29 +1,25 @@
 package com.sneltyn.demopreload;
 
 import android.content.Context;
+import android.media.session.PlaybackState;
 import android.net.Uri;
 import android.os.Handler;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.RelativeLayout;
-
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.extractor.mp4.Mp4Extractor;
 import com.google.android.exoplayer2.source.ClippingMediaSource;
 import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
-import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.source.smoothstreaming.DefaultSsChunkSource;
 import com.google.android.exoplayer2.source.smoothstreaming.SsMediaSource;
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DataSpec;
@@ -33,260 +29,234 @@ import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.HttpDataSource;
 import com.google.android.exoplayer2.upstream.RawResourceDataSource;
 import com.google.android.exoplayer2.util.Util;
-
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.concurrent.TimeUnit;
 
 public class DefaultPlayer
-        extends
-        RelativeLayout {
+    extends
+    RelativeLayout {
 
-    public static final String TAG  = "SNELTYN";
+  public static final String TAG = "SNELTYN";
 
-    private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+  private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
 
-    private Handler mHandler;
-    private PlayerView playerView;
-    private DefaultDataSourceFactory mMediaDataSourceFactory;
-    private SimpleExoPlayer mPlayer;
-    private DefaultTrackSelector mTrackSelector;
-    private String mUserAgent;
-    private PlayerEventListener mPlayerListener;
-    private ConcatenatingMediaSource mConcatVideos;
+  private final ArrayList<String> urls = new ArrayList<>();
+  private int k = 0;
+  private Handler mHandler;
+  private PlayerView playerView;
+  private DefaultDataSourceFactory mDataSourceFactory;
+  private SimpleExoPlayer mPlayer;
+  private String mUserAgent;
+  private ConcatenatingMediaSource mConcatVideos;
+  private final Deque<String> mQue = new LinkedList<>();
 
-    private boolean ended;
+  public DefaultPlayer(Context context) {
+    super(context);
+    init();
+  }
 
-    public DefaultPlayer(Context context) {
-        super(context);
-        init();
+  public DefaultPlayer(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    init();
+  }
+
+  public DefaultPlayer(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+    init();
+  }
+
+  private void init() {
+    inflate(getContext(), R.layout.view_player, this);
+    mHandler = new Handler();
+    playerView = findViewById(R.id.video_render);
+
+    DataSpec dataSpec = new DataSpec(RawResourceDataSource.buildRawResourceUri(R.raw.test));
+    final RawResourceDataSource rawResourceDataSource = new RawResourceDataSource(getContext());
+    try {
+      rawResourceDataSource.open(dataSpec);
+      urls.add(rawResourceDataSource.getUri().toString());
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    urls.add("https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8");
+    mUserAgent = Util.getUserAgent(getContext(), "snplayer");
+    mDataSourceFactory = new DefaultDataSourceFactory(getContext(),
+        mUserAgent, BANDWIDTH_METER);
+    mConcatVideos = new ConcatenatingMediaSource();
+    initializePlayer();
+  }
 
-    public DefaultPlayer(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init();
-    }
+  public void nextPlay() {
+    playVideo(urls.get(k++), 10);
+    k %= urls.size();
+  }
 
-    public DefaultPlayer(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init();
-    }
+  public void playVideo(int resourceId) {
+    try {
 
-    private void init() {
-        inflate(getContext(), R.layout.view_player, this);
-        mHandler = new Handler();
-        playerView = findViewById(R.id.video_render);
-        mUserAgent = Util.getUserAgent(getContext(), "snplayer");
-        mMediaDataSourceFactory = new DefaultDataSourceFactory(getContext(),
-                mUserAgent, BANDWIDTH_METER);
-        mTrackSelector = new DefaultTrackSelector(
-                new AdaptiveTrackSelection.Factory(BANDWIDTH_METER));
-        mConcatVideos = new ConcatenatingMediaSource();
-        mPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), mTrackSelector);
-        mPlayerListener = new PlayerEventListener();
-        mPlayer.addListener(mPlayerListener);
-        playerView.setPlayer(mPlayer);
-        playerView.hideController();
-        playerView.setUseController(false);
-        playerView.requestFocus();
-    }
+      DataSpec dataSpec = new DataSpec(RawResourceDataSource.buildRawResourceUri(resourceId));
+      final RawResourceDataSource rawResourceDataSource = new RawResourceDataSource(getContext());
+      rawResourceDataSource.open(dataSpec);
 
-
-    public void preloadVideo(String path) {
-        try {
-            MediaSource source = buildMediaSource(Uri.parse(path));
-            if(mConcatVideos.getSize() == 0)
-                mConcatVideos = new ConcatenatingMediaSource();
-            mConcatVideos.addMediaSource(source);
-        } catch (Exception ignore) {
-           Log.e(TAG, "Problem preload video: " + path);
-        }
-    }
-
-    public void preloadVideo(String path, int duration) {
-        try {
-            MediaSource source = buildMediaSource(Uri.parse(path));
-            if (mConcatVideos.getSize() == 0)
-                mConcatVideos = new ConcatenatingMediaSource();
-            mConcatVideos.addMediaSource(new ClippingMediaSource(source, 0,
-                    TimeUnit.SECONDS.toMicros(duration)));
-        } catch (Exception ignore) {
-            Log.e(TAG, "Problem play stream: " + path);
-        }
-    }
-
-    public void playVideo(int resourceId){
-        try {
-
-            DataSpec dataSpec = new  DataSpec(RawResourceDataSource.buildRawResourceUri(resourceId));
-            final RawResourceDataSource rawResourceDataSource = new RawResourceDataSource(getContext());
-            rawResourceDataSource.open(dataSpec);
-
-            DataSource.Factory factory = new DataSource.Factory() {
-                @Override
-                public DataSource createDataSource() {
-                    return rawResourceDataSource;
-                }
-            };
-
-            mConcatVideos = new ConcatenatingMediaSource();
-            MediaSource source = new ExtractorMediaSource.Factory(factory)
-                    .createMediaSource(rawResourceDataSource.getUri());
-            mConcatVideos.addMediaSource(source);
-            mPlayer.setPlayWhenReady(true);
-            mPlayer.prepare(mConcatVideos);
-        } catch (Exception ignored) {
-            Log.e(TAG, "Problem play video: " + resourceId);
-        }
-    }
-
-    public void playVideo(String path) {
-        try {
-            mConcatVideos = new ConcatenatingMediaSource();
-            MediaSource source = buildMediaSource(Uri.parse(path));
-            mConcatVideos.addMediaSource(source);
-            mPlayer.setPlayWhenReady(true);
-            mPlayer.prepare(mConcatVideos);
-        } catch (Exception ignored) {
-            Log.e(TAG, "Problem play video: " + path);
-        }
-    }
-
-    public void playVideo(String path, int duration) {
-        try {
-            mConcatVideos = new ConcatenatingMediaSource();
-            MediaSource source = buildMediaSource(Uri.parse(path));
-            mConcatVideos.addMediaSource(new ClippingMediaSource(source, 0, TimeUnit.SECONDS.toMicros(duration)));
-            mPlayer.setPlayWhenReady(true);
-            mPlayer.prepare(mConcatVideos);
-        } catch (Exception ignored) {
-            Log.e(TAG, "Problem play stream: " + path);
-        }
-    }
-
-
-    public void stop() {
-        mPlayer.stop();
-        removeCallback();
-    }
-
-    public void release() {
-        stop();
-        playerView.setPlayer(null);
-        mPlayer.removeListener(mPlayerListener);
-        mPlayer.release();
-        mPlayer = null;
-        mTrackSelector = null;
-    }
-
-    private MediaSource buildMediaSource(
-            Uri uri) {
-        int type = Util.inferContentType(uri);
-        switch (type) {
-            case C.TYPE_DASH:
-                return new DashMediaSource.Factory(
-                        new DefaultDashChunkSource.Factory(mMediaDataSourceFactory),
-                        buildDataSourceFactory())
-                        .createMediaSource(uri);
-            case C.TYPE_SS:
-                return new SsMediaSource.Factory(
-                        new DefaultSsChunkSource.Factory(mMediaDataSourceFactory),
-                        buildDataSourceFactory())
-                        .createMediaSource(uri);
-            case C.TYPE_HLS:
-                return new HlsMediaSource.Factory(mMediaDataSourceFactory)
-                        .createMediaSource(uri);
-            case C.TYPE_OTHER:
-                return new ExtractorMediaSource.Factory(mMediaDataSourceFactory)
-                        .createMediaSource(uri);
-            default: {
-                throw new IllegalStateException("Unsupported type: " + type);
-            }
-        }
-    }
-
-    public void removeCallback() {
-        mHandler.removeCallbacksAndMessages(null);
-    }
-
-    private DataSource.Factory buildDataSourceFactory() {
-        return new DefaultDataSourceFactory(getContext(), null,
-                buildHttpDataSourceFactory());
-    }
-
-    private HttpDataSource.Factory buildHttpDataSourceFactory() {
-        return new DefaultHttpDataSourceFactory(mUserAgent, null);
-    }
-
-    private class PlayerEventListener extends Player.DefaultEventListener {
-
+      DataSource.Factory factory = new DataSource.Factory() {
         @Override
-        public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-            if (playbackState == Player.STATE_READY) {
-                preloadVideo("https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8", 10);
-            } else if (playbackState == Player.STATE_ENDED) {
-                ended = true;
-                Log.d(TAG, "STATE_ENDED");
-                mConcatVideos.releaseSourceInternal();
-                mConcatVideos.clear();
-                stop();
-            }
+        public DataSource createDataSource() {
+          return rawResourceDataSource;
         }
+      };
 
-        @Override
-        public void onPositionDiscontinuity(int reason) {
-            ended = false;
-            //super.onPositionDiscontinuity(reason);
-            //println(DEBUG, MediaView.class, "WTF? O_o");
-            Log.d(TAG, "onPositionDiscontinuity");
-            mConcatVideos.removeMediaSource(0);
-        }
-
-        @Override
-        public void onPlayerError(ExoPlaybackException error) {
-
-            error.printStackTrace();
-
-            //super.onPlayerError(error);
-            //println(DEBUG, MediaView.class, "ExoPlayer error: %s", error.printStackTrace(););
-            //printException(error.fillInStackTrace());
-            //mListener.onCompletion();
-
-//            String errorString = null;
-//            if (e.type == ExoPlaybackException.TYPE_RENDERER) {
-//                Exception cause = e.getRendererException();
-//                if (cause instanceof DecoderInitializationException) {
-//                    // Special case for decoder initialization failures.
-//                    DecoderInitializationException decoderInitializationException =
-//                            (DecoderInitializationException) cause;
-//                    if (decoderInitializationException.decoderName == null) {
-//                        if (decoderInitializationException.getCause() instanceof DecoderQueryException) {
-//                            errorString = getString(R.string.error_querying_decoders);
-//                        } else if (decoderInitializationException.secureDecoderRequired) {
-//                            errorString = getString(R.string.error_no_secure_decoder,
-//                                    decoderInitializationException.mimeType);
-//                        } else {
-//                            errorString = getString(R.string.error_no_decoder,
-//                                    decoderInitializationException.mimeType);
-//                        }
-//                    } else {
-//                        errorString = getString(R.string.error_instantiating_decoder,
-//                                decoderInitializationException.decoderName);
-//                    }
-//                }
-//            }
-//            if (errorString != null) {
-//                showToast(errorString);
-//            }
-//            inErrorState = true;
-//            if (isBehindLiveWindow(e)) {
-//                clearResumePosition();
-//                initializePlayer();
-//            } else {
-//                updateResumePosition();
-//                updateButtonVisibilities();
-//                showControls();
-//            }
-        }
+      mConcatVideos = new ConcatenatingMediaSource();
+      MediaSource source = new ExtractorMediaSource.Factory(factory)
+          .createMediaSource(rawResourceDataSource.getUri());
+      mConcatVideos.addMediaSource(source);
+      mPlayer.setPlayWhenReady(true);
+      mPlayer.prepare(mConcatVideos);
+    } catch (Exception ignored) {
+      Log.d(TAG, "Problem play video: " + resourceId);
     }
+  }
+
+  public void preloadVideo(String path, double duration) {
+    if (mConcatVideos.getSize() == 0) return;
+    Log.d(TAG, "Preload video: " + path);
+    try {
+      MediaSource source = buildMediaSource(Uri.parse(path));
+      int seconds = (int) duration;
+      int millis = (int) ((duration - seconds) * 1000) + seconds * 1000;
+      mConcatVideos.addMediaSource(new ClippingMediaSource(source, 0,
+          TimeUnit.MILLISECONDS.toMicros(millis)));
+      mQue.add(path);
+    } catch (Exception ignore) {
+      Log.d(TAG, "Problem preload video: " + path);
+    }
+  }
+
+  public void playVideo(String path, double duration) {
+    Log.d(TAG, "Play video: " + path);
+    if (mConcatVideos.getSize() > 0
+        && path.equals(mQue.peek())
+        && mPlayer.getPlaybackState() == PlaybackState.STATE_PLAYING) {
+      Log.d(TAG, "PRELOADED VIDEO");
+      return;
+    }
+
+    try {
+      Log.d(TAG, "WITHOUT PRELOAD");
+      mQue.clear();
+      mConcatVideos.clear();
+      MediaSource source = buildMediaSource(Uri.parse(path));
+      int seconds = (int) duration;
+      int millis = (int) ((duration - seconds) * 1000) + seconds * 1000;
+      mConcatVideos.addMediaSource(
+          new ClippingMediaSource(source, 0, TimeUnit.MILLISECONDS.toMicros(millis)));
+      mQue.add(path);
+      mPlayer.setVolume(1);
+      mPlayer.prepare(mConcatVideos);
+      mPlayer.setPlayWhenReady(true);
+    } catch (Exception ex) {
+      Log.d(TAG, "Problem preload video: " + path);
+    }
+  }
+
+  private void initializePlayer() {
+    if (mPlayer != null) return;
+    mPlayer = ExoPlayerFactory.newSimpleInstance(getContext());
+    mConcatVideos = new ConcatenatingMediaSource();
+    mPlayer.addListener(new PlayerEventListener());
+    playerView.setPlayer(mPlayer);
+    playerView.hideController();
+    playerView.setUseController(false);
+    playerView.requestFocus();
+  }
+
+  private void releasePlayer() {
+    if (mPlayer == null) return;
+    removeCallback();
+    mPlayer.release();
+    mPlayer = null;
+    mConcatVideos = null;
+    mQue.clear();
+  }
+
+  private MediaSource buildMediaSource(Uri uri) {
+    int type = Util.inferContentType(uri);
+    switch (type) {
+      case C.TYPE_DASH:
+        return new DashMediaSource.Factory(mDataSourceFactory).createMediaSource(uri);
+      case C.TYPE_SS:
+        return new SsMediaSource.Factory(new DefaultSsChunkSource.Factory(mDataSourceFactory),
+            buildDataSourceFactory())
+            .createMediaSource(uri);
+      case C.TYPE_HLS:
+        return new HlsMediaSource.Factory(mDataSourceFactory)
+            .setAllowChunklessPreparation(true)
+            .createMediaSource(uri);
+      case C.TYPE_OTHER:
+        return new ExtractorMediaSource.Factory(mDataSourceFactory).createMediaSource(uri);
+      default: {
+        throw new IllegalStateException("Unsupported type: " + type);
+      }
+    }
+  }
+
+  public void removeCallback() {
+    mHandler.removeCallbacksAndMessages(null);
+  }
+
+  private DataSource.Factory buildDataSourceFactory() {
+    return new DefaultDataSourceFactory(getContext(), null,
+        buildHttpDataSourceFactory());
+  }
+
+  private void nextPreload() {
+    preloadVideo(urls.get(k++), 10);
+    k %= urls.size();
+  }
+
+  private HttpDataSource.Factory buildHttpDataSourceFactory() {
+    return new DefaultHttpDataSourceFactory(mUserAgent, null);
+  }
+
+  private class PlayerEventListener implements Player.EventListener {
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+      Log.d(TAG, "STATE playWhenReady = " + playWhenReady + ", "
+          + "playbackState = " + playbackState);
+
+      if (playbackState == Player.STATE_READY) {
+        nextPreload();
+      } else if (playbackState == Player.STATE_ENDED) {
+        Log.d(TAG, "STATE_ENDED");
+        nextPlay();
+      }
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+      //super.onPositionDiscontinuity(reason);
+      //println(DEBUG, MediaView.class, "WTF? O_o");
+      Log.d(TAG, "onPositionDiscontinuity");
+      if (mConcatVideos.getSize() > 0) {
+        mConcatVideos.removeMediaSource(0);
+        mQue.remove();
+      }
+      nextPreload();
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+      error.printStackTrace();
+      mConcatVideos.clear();
+      mQue.clear();
+      
+      //releasePlayer();
+      //initializePlayer();
+      nextPlay();
+    }
+  }
 }
--- app/src/main/java/com/sneltyn/demopreload/MainActivity.kt
+++ app/src/main/java/com/sneltyn/demopreload/MainActivity.kt
@@ -3,25 +3,19 @@ package com.sneltyn.demopreload
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity(){
+  private lateinit var mPlayer: DefaultPlayer
 
-    private lateinit var mPlayer: DefaultPlayer
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    mPlayer = findViewById(R.id.player)
+  }
 
-        mPlayer = findViewById(R.id.player)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        mPlayer.playVideo(R.raw.test)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mPlayer.release()
-    }
+  override fun onStart() {
+    super.onStart()
+    mPlayer.nextPlay()
+  }
 }
